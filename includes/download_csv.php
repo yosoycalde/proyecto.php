6640704f7e4ed2f26536aea58fe00e68c9c85c64
yosoycalde <juancalderon2005.18@gmail.com>
2025-08-01 10:03:05 -0500

    terminado-preparando para la actualizacion

--- includes/download_csv.php
+++ includes/download_csv.php
@@ -136,23 +136,6 @@ function mostrarErrorDescarga($mensaje)
 {
     include "config/error.html"
     ?>
-
-        <script>
-            setTimeout(function () {
-                fetch('../includes/cleanup.php', { method: 'POST' })
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.success) {
-                            console.log(' Limpieza automática completada');
-                        }
-                    }).catch(error => {
-                        console.log(' Error en limpieza automática:', error);
-                    });
-            }, 3000);
-        </script>
-    </body>
-
-    </html>
     <?php
 }
 ?>
---
+++ js/download01.js
@@ -0,0 +1,11 @@
+setTimeout(function () {
+    fetch('../includes/cleanup.php', { method: 'POST' })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                console.log(' Limpieza automática completada');
+            }
+        }).catch(error => {
+            console.log(' Error en limpieza automática:', error);
+        });
+}, 3000);
